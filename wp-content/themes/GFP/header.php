9665e9e58a351bc3b9a6b189a1c754c72ac64a3b
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-02-23 12:45:32 -0500

    init

---
+++ wp-content/themes/GFP/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Green Farm Parts</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet">
+    <link rel="stylesheet" href="/v/vspfiles/templates/gfp-test/css/style.css">
+</head>
+<body <?php body_class(); ?>>
+    
+    <!-- EYEBROW -->
+    <section class="eyebrow">
+        <div class="site-width">
+            <ul class="eyebrow-links">
+                <li>Free Shipping Over $50 &amp; Hassle Free Returns <a href="#0">Details</a></li>
+                <li><a href="#0">Green Farm Parts Exclusives</a></li>
+                <li><a href="#0">Chat</a></li>
+                <li><a href="#0">Why Green Farm Parts?</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <!-- SITE-HEADER -->
+    <header class="site-header">
+        <div class="site-width">
+            <nav>
+                <ul class="site-header-list">
+                    <li class="site-header-logo-container"><a href="/"><img src="/v/vspfiles/templates/gfp-test/img/GFP-logo.png" alt=""></a></li>
+                    <li><a href="#0">Gifts</a></li>
+                    <li><a href="#0">Deals &amp; Promotions</a></li>
+                    <li><a href="#0">My Account</a></li>
+                    <li><a href="#0">Order Status</a></li>
+                    <li class="shopping-cart-list-item"><a href="#0"><img src="/v/vspfiles/templates/gfp-test/img/cart-icon.jpg" alt=""></a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!-- MAIN CONTAINER -->
+    <main>
